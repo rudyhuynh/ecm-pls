8c5280c836adac3cdebf81c0f9ea4f3b0afc5475
rudyhuynh <>
2025-07-24 00:56:15 +0700

    init

---
+++ README.md
@@ -0,0 +1,20 @@
+# Data Analysis
+
+## Cài đặt
+
+- Máy tính ở local cần có:
+  - R phiên bản >= 4.5.1 (https://www.r-project.org/)
+  - RStudio IDE (https://posit.co/download/rstudio-desktop/)
+- Clone source code bằng lệnh `git clone https://github.com/rudyhuynh/ecm-pls.git` hoặc tải trực tiếp từ Github.
+
+## Các bước thực thi
+
+- Copy file data `Bang_Khao_Sat_Import_Google_Form - Form Responses 1.csv` tải về từ Google Form vào cùng thư mục chứa file `README.md` này.
+- Chạy lệnh `Rscript src/handle_data.R` để xử lý và làm sạch dữ liệu từ Google Form --> output: `data.csv`
+- Import `data.csv` và `src/studio.R` vào RStudio IDE.
+- Chạy các lệnh cần thiết trong `studio.R` bằng RStudio IDE để phân tích mô hình và plot model.
+
+## Reference
+
+- Hair Jr, J. F., Hult, G. T. M., Ringle, C. M., Sarstedt, M., Danks, N. P., & Ray, S. (2021). Partial least squares structural equation modeling (PLS-SEM) using R: A workbook. Springer Nature
+  https://doi.org/10.1007/978-3-030-80519-7
---
+++ src/studio.R
@@ -0,0 +1,54 @@
+# Code trong file này được sử dụng cho RStudio thay vì chạy bằng lệnh Rscript.
+
+library(dplyr)
+library(seminr)
+library(DiagrammeR)
+
+set.seed(2025)
+
+measurement_model <- constructs(
+  composite("PU", multi_items("PU", 1:5)),
+  composite("PEOU", multi_items("PEOU", c(1,2,4))),
+  composite("HAB", multi_items("HAB", 1:3)),
+  composite("SAT", multi_items("SAT", 1:2)),
+  composite("CONF", multi_items("CONF", 1:3)),
+  composite("PI", multi_items("PI", 1:2))
+)
+structural_model <- relationships(
+  paths(from = "PU", to = "PI"), # H1
+  paths(from = "PEOU", to = "PI"), # H2
+  paths(from = "SAT", to = "PI"), # H3
+  paths(from = "HAB", to = "PI"), # H4
+  paths(from = "PU", to = "SAT"), # H5
+  paths(from = "PEOU", to = "SAT"), # H6
+  paths(from = "CONF", to = "SAT"), # H7
+  paths(from = "PEOU", to = "PU"), # H8
+  paths(from = "CONF", to = "PU"), # H9
+  paths(from = "CONF", to = "PEOU") # H10
+)
+
+model <- estimate_pls(data = data,
+                      measurement_model = measurement_model,
+                      structural_model = structural_model,
+                      inner_weights = path_weighting,
+                      missing = mean_replacement,
+                      missing_value = "-99")
+
+summ <- summary(model)
+
+bs_model <- bootstrap_model(seminr_model = model, nboot = 10000, cores = NULL)
+
+# Dùng để kiểm tra mô hình đo lường:
+summ$loadings
+summ$reliability
+summ$validity$htmt
+
+# Dùng để kiểm tra mô hình cấu trúc:
+summ$vif_antecedents
+summary_boot$bootstrapped_paths
+summary_boot$bootstrapped_total_paths
+
+# Plot the models:
+plot(measurement_model)
+plot(structural_model)
+plot(bs_model, title = "PLS Model")
